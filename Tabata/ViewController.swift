9491fe53df66ee93bbe7c0bd0fbcb0cc6c2f6ddb
BigBackBoom <kodai.kikuchi@uqconnect.edu.au>
2015-03-28 17:12:19 +1000

    still messed up but dynamically creating label and timerpicker is examined

--- Tabata/ViewController.swift
+++ Tabata/ViewController.swift
@@ -10,21 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var circleProgressView: CircleProgressView!
-    @IBOutlet weak var progressLabel: UILabel!
-    @IBOutlet weak var progressSlider: UISlider!
-    @IBOutlet weak var clockwiseSwitch: UISwitch!
+    @IBOutlet var circleProgressView: CircleProgressView!
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var timePicker: UIDatePicker!
+    
+    weak var timer: NSTimer!
+    var timerLabel = UILabel();
+    var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //git test2
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func starBtnPressed(){
+        println(timePicker.countDownDuration)
+        timerLabel.frame = CGRectMake(50, 150, 200, 21)
+        timerLabel.backgroundColor = UIColor.orangeColor()
+        timerLabel.textColor = UIColor.blackColor()
+        timerLabel.textAlignment = NSTextAlignment.Center
+        timerLabel.text = "test label"
+        self.view.addSubview(timerLabel)
+        //timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("timerIncrement"), userInfo: nil, repeats: false)
+    }
+    
+    func timerIncrement (){
+        counter++;
+        timerLabel.text = String(counter)
+        
+        if(counter != 0){
+            NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("timerIncrement"), userInfo: nil, repeats: false)
+        }
+    }
+    
+    @IBAction func pauseButton(sender: AnyObject) { }
+    @IBAction func clearButton(sender: AnyObject) { }
 
 
 }
