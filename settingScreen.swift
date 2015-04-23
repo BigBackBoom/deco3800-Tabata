dc70f6ee85d447350d82a7c1686d488fa6b0c28b
BigBackBoom <kodai.kikuchi@uqconnect.edu.au>
2015-04-24 09:57:01 +1000

    created setting class

---
+++ settingScreen.swift
@@ -0,0 +1,22 @@
+//
+//  settingScreen.swift
+//  Tabata
+//
+//  Created by Kodai Kikuchi on 2015/04/24.
+//  Copyright (c) 2015年 deco3800. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class settingScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
