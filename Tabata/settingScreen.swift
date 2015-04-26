fa4ca3c250693c1511ecb817518416436c551d5b
BigBackBoom <kodai.kikuchi@uqconnect.edu.au>
2015-04-26 19:22:59 +1000

    basic setting is partially working

--- Tabata/settingScreen.swift
+++ Tabata/settingScreen.swift
@@ -11,17 +11,21 @@ import UIKit
 
 class settingScreen: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var exeVal: UILabel!
-    var restVal: UILabel!
-    var soundVal: UILabel!
-    var cycleVal: UILabel!
+    var exeVal: UITextField!
+    var restVal: UITextField!
+    var soundVal: UITextField!
+    var cycleVal: UITextField!
     
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var navBarSetting: UINavigationItem!
     var items: [String] = ["Excercise", "Rest", "Sound", "help"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        let customBackButton: UIBarButtonItem = UIBarButtonItem(title: "< Back", style: .Plain, target: self, action: Selector("savedata"))
+        navBarSetting.leftBarButtonItem = customBackButton
+
     }
         
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -31,35 +35,56 @@ class settingScreen: UIViewController, UITableViewDelegate, UITableViewDataSourc
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
         cell.textLabel?.text = self.items[indexPath.row]
-        
-        switch indexPath.row{
-            case 0:
-                exeVal = UILabel()
-                exeVal.text = String(stringInterpolationSegment: exeSetting.exeTime)
-                exeVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
-                cell.addSubview(exeVal)
-            case 1:
-                restVal = UILabel()
-                restVal.text =  String(stringInterpolationSegment: exeSetting.restTime)
-                restVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
-                cell.addSubview(restVal)
-            case 2:
-                soundVal = UILabel()
-                soundVal.text =  String(stringInterpolationSegment: exeSetting.sound)
-                soundVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
-                cell.addSubview(soundVal)
-            case 3:
-                cycleVal = UILabel()
-                cycleVal.text =  String(exeSetting.cycle)
-                cycleVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
-                cell.addSubview(cycleVal)
-            default:
-                println("default");
-        }
+        createTable(cell, row:indexPath.row)
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         println("You selected cell #\(indexPath.row)!")
     }
+    
+    @objc func savedata(){
+        exeSetting.exeTime = (exeVal.text as NSString).doubleValue
+        exeSetting.restTime = (exeVal.text as NSString).doubleValue
+        exeSetting.sound = (exeVal.text as NSString).doubleValue
+        exeSetting.cycle = (exeVal.text as NSString).integerValue
+        print(exeSetting)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let startScreen = storyboard.instantiateViewControllerWithIdentifier("startScreen") as! UIViewController
+        presentViewController(startScreen, animated: true, completion: nil)
+        print(exeSetting)
+
+    }
+    
+    private func createTable (cell:UITableViewCell, row: Int){
+        
+        switch row{
+        case 0:
+            exeVal = UITextField()
+            exeVal.keyboardType = UIKeyboardType.NumberPad
+            exeVal.text = String(stringInterpolationSegment: exeSetting.exeTime)
+            exeVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
+            cell.addSubview(exeVal)
+        case 1:
+            restVal = UITextField()
+            restVal.text =  String(stringInterpolationSegment: exeSetting.restTime)
+            restVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
+            cell.addSubview(restVal)
+        case 2:
+            soundVal = UITextField()
+            soundVal.keyboardType = UIKeyboardType.NumberPad
+            soundVal.text =  String(stringInterpolationSegment: exeSetting.sound)
+            soundVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
+            cell.addSubview(soundVal)
+        case 3:
+            cycleVal = UITextField()
+            cycleVal.keyboardType = UIKeyboardType.NumberPad
+            cycleVal.text =  String(exeSetting.cycle)
+            cycleVal.frame = CGRectMake((cell.frame.size.width - 30), 0, 100, 50)
+            cell.addSubview(cycleVal)
+        default:
+            println("default");//need error checking
+        }
+    }
+    
 }
